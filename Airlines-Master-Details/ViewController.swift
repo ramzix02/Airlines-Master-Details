6fb649d85b59d9b8fa61d2a2d96ba88176370a46
Ahmed Ramzy <ahmed.ramzy@innovitics.com>
2021-09-21 10:39:46 +0200

    Initial Commit

---
+++ Airlines-Master-Details/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Airlines-Master-Details
+//
+//  Created by Ahmed Ramzy on 21/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
